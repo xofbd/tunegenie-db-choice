e9824de945dab7151baf0a67af8e3996785610f6
Don Fox <dfox09@gmail.com>
2024-01-09 11:47:29 -0500

    Load env variables with dotenv

--- src/upload_data.py
+++ src/upload_data.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-
+from dotenv import load_dotenv
 from tune_genie import get_song_data
 
+load_dotenv()
 
 payload = get_song_data(datetime(2023, 12, 10))
 print(payload)
